272f2b8d181c69d379379b3bc1bd4c6411393f3b
Sergey Bolshchikov <sergey@bolshchikov.net>
2022-03-29 11:41:29 +0300

    feat(mapper): add ops ticket to description closes #26

--- src/mapper/map-description.ts
+++ src/mapper/map-description.ts
@@ -4,5 +4,9 @@ import { clearFromHTML } from '../utils/sanitize-text';
 
 export const mapDescription = (mondayTask: TMondayItem, to: TCsvIssue): TCsvIssue => {
   to.description = clearFromHTML(mondayTask.updates[0]?.body ?? '');
+  const opsTicket = mondayTask.column_values.find(c => c.title === 'JIRA');
+  if (opsTicket && opsTicket.text) {
+    to.description += `\nOps ticket: ${opsTicket.text}`
+  }
   return to;
 };
